ed6ad0923bab00869ecb6284fdd9e9dc75e4cc4b
Lucas Pires <lucas.pires@epfl.ch>
2015-10-20 22:44:57 +0200

    Adds javadoc, clean some parts (adds string constants for the various "virtual fields type"), and complete JSON code of Group

--- android/Calamar/app/src/main/java/ch/epfl/sweng/calamar/Item.java
+++ android/Calamar/app/src/main/java/ch/epfl/sweng/calamar/Item.java
@@ -5,6 +5,14 @@ import org.json.JSONObject;
 
 import java.util.Date;
 
+/**
+ * Models an Item, superclass of all the possibly many kind of 'item' the app manage. <br><br>
+ *     known subclasses : <li>
+ *                          <ul>{@link SimpleTextItem},</ul>
+ *                          <ul>{@link LocalizedItem}</ul>
+ *                        </li>
+ *     Item is immutable
+ */
 public abstract class Item {
     private final int ID;
     private final User from;
@@ -23,29 +31,62 @@ public abstract class Item {
     }
 
     //TODO: maybe better field names in java...
+
+    /**
+     * @return the 'from' field of the Item (sender)
+     */
     public User getFrom() {
         return from;
     }
 
+    /**
+     * @return the 'to' field of the Item (recipient)
+     */
     public Recipient getTo() {
         return to;
     }
 
+    /**
+     * @return the creation/posting date of the Item
+     */
     public Date getDate() {
         return new Date(this.date);
     }
 
     //TODO: hashcode equals
 
-    protected void compose(JSONObject o) throws JSONException {
-        o.accumulate("ID", ID);
-        o.accumulate("from", from.toJSON());
-        o.accumulate("to", to.toJSON());
-        o.accumulate("date", date);
+
+    /**
+     * Appends the fields of {@link Item} to a {@link JSONObject} representing the Item.<br>
+     *     is called by the {@link #compose(JSONObject)} method of the child classes in
+     *     a chain where each compose method append the field of its class to the object.<br>
+     *         The chain begins by a call to {@link #toJSON()} in an instantiable child class.<br><br>
+     * Should <b>NOT</b> be used alone.
+     * @param json the json to which we append (using {@link JSONObject#accumulate(String, Object)} ) data
+     * @throws JSONException
+     */
+    protected void compose(JSONObject json) throws JSONException {
+        json.accumulate("ID", ID);
+        json.accumulate("from", from.toJSON());
+        json.accumulate("to", to.toJSON());
+        json.accumulate("date", date);
     }
 
+    /**
+     * @return a JSONObject representing a {@link Item)
+     * @throws JSONException
+     */
+    //must remains abstract if class not instantiable
     public abstract JSONObject toJSON() throws JSONException;
 
+    /**
+     * Parses an Item from a JSONObject.<br>
+     * To instantiate the correct Item ({@link SimpleTextItem}, etc ...)
+     * the JSON must have a 'type' field indicating the type...('simpleText', ...)
+     * @param json the well formed {@link JSONObject json} representing the {@link Item item}
+     * @return a {@link Item item} parsed from the JSONObject
+     * @throws JSONException
+     */
     public static Item fromJSON(JSONObject json) throws JSONException, IllegalArgumentException {
         if (null == json || json.isNull("type")) {
             throw new IllegalArgumentException("malformed json, either null or no 'type' value");
@@ -62,6 +103,11 @@ public abstract class Item {
         return item;
     }
 
+    /**
+     * A Builder for {@link Item}, has no build() method since Item isn't instantiable,
+     * is used by the child builders (in {@link SimpleTextItem} or...) to build the "Item
+     * part of the object". currently only used to parse JSON (little overkill..but ..)
+     */
     protected static class Builder {
         protected int ID;
         protected User from;
--- android/Calamar/app/src/main/java/ch/epfl/sweng/calamar/User.java
+++ android/Calamar/app/src/main/java/ch/epfl/sweng/calamar/User.java
@@ -3,23 +3,49 @@ package ch.epfl.sweng.calamar;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+/**
+ * Models an user, (kind of {@link Recipient}).
+ */
 public final class User extends Recipient {
-    //type = user, virtual field ^^
+    private final static String RECIPIENT_TYPE = "user";
 
+    /**
+     * Instantiates a new User with <i>ID</i> and <i>name</i>
+     * @param ID , the id
+     * @param name , the name of the user ("Bob"..)
+     */
     public User(int ID, String name) {
         super(ID, name);
     }
 
+    /**
+     * Appends the fields of User (currently nothing but the "virtual" field type = RECIPIENT_TYPE)
+     * to the given JSONObject. <br><br>
+     *     Should <b>NOT</b> be used alone
+     * @param json the json to which we append data
+     * @throws JSONException
+     */
     @Override
     protected void compose(JSONObject json) throws JSONException {
-        super.compose(json);
-        json.accumulate("type", "user");
+        super.compose(json);//adds parent fields
+        json.accumulate("type", User.RECIPIENT_TYPE);
     }
 
+    /**
+     * Parses a User from a JSONObject.<br>
+     * @param json the well formed {@link JSONObject json} representing the {@link User user}
+     * @return a {@link User user} parsed from the JSONObject
+     * @throws JSONException
+     * @see ch.epfl.sweng.calamar.Recipient#fromJSON(JSONObject) Recipient.fromJSON
+     */
     public static User fromJSON(JSONObject json) throws JSONException {
         return new User.Builder().parse(json).build();
     }
 
+    /**
+     * @return a JSONObject representing a {@link User}
+     * @throws JSONException
+     */
     @Override
     public JSONObject toJSON() throws JSONException {
         JSONObject json = new JSONObject();
@@ -27,9 +53,17 @@ public final class User extends Recipient {
         return json;
     }
 
+    /**
+     * A Builder for {@link User}, currently only used to parse JSON (little overkill..but ..)
+     * @see ch.epfl.sweng.calamar.Recipient.Builder
+     */
     private static class Builder extends Recipient.Builder {
         public Builder parse(JSONObject json) throws JSONException {
             super.parse(json);
+            String type = json.getString("type");
+            if(!type.equals(User.RECIPIENT_TYPE)) {
+                throw new IllegalArgumentException("expected "+User.RECIPIENT_TYPE+" was : " + type);
+            }
             return this;
         }
         public User build() {
