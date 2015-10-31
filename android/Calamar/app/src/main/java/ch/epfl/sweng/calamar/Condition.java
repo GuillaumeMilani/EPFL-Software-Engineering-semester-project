0bedf973f6fa1f68429807b7c5ef9570497be8d0
Pierre Quinton <pierre.quinton@epfl.ch>
2015-10-31 11:38:10 +0100

    added parsing to Condition

--- android/Calamar/app/src/main/java/ch/epfl/sweng/calamar/Condition.java
+++ android/Calamar/app/src/main/java/ch/epfl/sweng/calamar/Condition.java
@@ -1,5 +1,8 @@
 package ch.epfl.sweng.calamar;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by pierre on 10/27/15.
  */
@@ -10,12 +13,53 @@ public abstract class Condition {
      */
     public abstract boolean matches();
 
+    protected abstract void compose(JSONObject json) throws JSONException;
+
+    public JSONObject toJSON() throws JSONException {
+        JSONObject ret = new JSONObject();
+        this.compose(ret);
+        return ret;
+    }
+
+    public static Condition fromJSON(JSONObject json) throws JSONException, IllegalArgumentException {
+        if (null == json || json.isNull("type")) {
+            throw new IllegalArgumentException("malformed json, either null or no 'type' value");
+        }
+        Condition cond;
+        String type = json.getString("type");
+        switch(type) {
+            case "position":
+                cond = PositionCondition.fromJSON(json);
+                break;
+            case "and":
+                cond = and(fromJSON(json.getJSONObject("a")), fromJSON(json.getJSONObject("b")));
+                break;
+            case "or":
+                cond = or(fromJSON(json.getJSONObject("a")), fromJSON(json.getJSONObject("b")));
+                break;
+            case "not":
+                cond = not(fromJSON(json.getJSONObject("val")));
+                break;
+            default:
+                throw new IllegalArgumentException("Unexpected Item type (" + type + ")");
+        }
+        return cond;
+    }
+
+
     public static Condition and(final Condition c1, final Condition c2)
     {
         return new Condition() {
             @Override
             public boolean matches() {
                 return c1.matches() && c2.matches();
+            }
+
+            @Override
+            protected void compose(JSONObject json) throws JSONException {
+                json.accumulate("type", "and");
+                json.accumulate("a" , c1.toJSON());
+                json.accumulate("b", c2.toJSON());
             }
         };
     }
@@ -27,6 +71,13 @@ public abstract class Condition {
             public boolean matches() {
                 return c1.matches() || c2.matches();
             }
+
+            @Override
+            protected void compose(JSONObject json) throws JSONException {
+                json.accumulate("type", "or");
+                json.accumulate("a" , c1.toJSON());
+                json.accumulate("b" , c2.toJSON());
+            }
         };
     }
 
@@ -37,6 +88,19 @@ public abstract class Condition {
             public boolean matches() {
                 return !c.matches();
             }
+
+            @Override
+            protected void compose(JSONObject json) throws JSONException {
+                json.accumulate("type", "not");
+                json.accumulate("val" , c.toJSON());
+            }
         };
+    }
+
+    protected static class Builder {
+
+        public Builder parse(JSONObject o) throws JSONException {
+            return this;
+        }
     }
 }
--- android/Calamar/app/src/main/java/ch/epfl/sweng/calamar/PositionCondition.java
+++ android/Calamar/app/src/main/java/ch/epfl/sweng/calamar/PositionCondition.java
@@ -1,13 +1,44 @@
 package ch.epfl.sweng.calamar;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by pierre on 10/27/15.
  */
-public class PositionCondition implements Condition {
+public class PositionCondition extends Condition {
 
-    Override
-    public boolean match(Object o)
-    {
+    private final static String CONDITION_TYPE = "poisition";
+
+    //position + radius
+
+    @Override
+    public boolean matches() {
         return false;
+    }
+
+    @Override
+    protected void compose(JSONObject json) throws JSONException {
+
+    }
+
+    public static Condition fromJSON(JSONObject json) throws JSONException {
+        return new PositionCondition.Builder().parse(json).build();
+    }
+
+    public static class Builder extends Condition.Builder {
+
+        public Builder parse(JSONObject json) throws JSONException {
+            super.parse(json);
+            String type = json.getString("type");
+            if(!type.equals(PositionCondition.CONDITION_TYPE)) {
+                throw new IllegalArgumentException("expected "+ PositionCondition.CONDITION_TYPE +" was : "+type);
+            }
+            return this;
+        }
+
+        public PositionCondition build() {
+            return new PositionCondition();
+        }
     }
 }
