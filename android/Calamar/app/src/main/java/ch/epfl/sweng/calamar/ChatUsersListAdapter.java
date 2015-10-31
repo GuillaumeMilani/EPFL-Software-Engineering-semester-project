4c63daa77883c779d308487321cae3b6fa7c0a7a
Qjaquier <q.jaquier@gmail.com>
2015-10-31 14:47:28 +0100

    Set the text size dirrectly in the xml

--- android/Calamar/app/src/main/java/ch/epfl/sweng/calamar/ChatUsersListAdapter.java
+++ android/Calamar/app/src/main/java/ch/epfl/sweng/calamar/ChatUsersListAdapter.java
@@ -14,7 +14,6 @@ import java.util.List;
 public class ChatUsersListAdapter extends BaseAdapter {
     private final ArrayList<User> users;
     private final Activity context;
-    private final float TEXT_SIZE=20;
 
     public ChatUsersListAdapter(Activity context, List<User> users){
         this.users=new ArrayList<>(users);
@@ -49,7 +48,6 @@ public class ChatUsersListAdapter extends BaseAdapter {
         }
 
         holder.userName.setText(user.getName());
-        holder.userName.setTextSize(TEXT_SIZE);
         return convertView;
     }
 
