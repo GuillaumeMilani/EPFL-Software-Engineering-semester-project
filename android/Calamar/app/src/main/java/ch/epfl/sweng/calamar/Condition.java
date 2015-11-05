f0c6f9b47deb2ec7620159057a63617745dd776c
Pierre Quinton <pierre.quinton@epfl.ch>
2015-11-05 13:17:27 +0100

    added always true and always false condition, might be used when no condition is given or file

--- android/Calamar/app/src/main/java/ch/epfl/sweng/calamar/Condition.java
+++ android/Calamar/app/src/main/java/ch/epfl/sweng/calamar/Condition.java
@@ -13,14 +13,31 @@ public abstract class Condition {
      */
     public abstract boolean matches();
 
+    /**
+     * compose this Condition in the json object
+     * @param json jsonObject to put this in
+     * @throws JSONException
+     */
     protected abstract void compose(JSONObject json) throws JSONException;
 
+    /**
+     * get a JSON description of this
+     * @return JSONObject describing this
+     * @throws JSONException
+     */
     public JSONObject toJSON() throws JSONException {
         JSONObject ret = new JSONObject();
         this.compose(ret);
         return ret;
     }
 
+    /**
+     * create a Condition from a JSONObject
+     * @param json Object in JSON format
+     * @return the desired condition Condition
+     * @throws JSONException
+     * @throws IllegalArgumentException
+     */
     public static Condition fromJSON(JSONObject json) throws JSONException, IllegalArgumentException {
         if (null == json || json.isNull("type")) {
             throw new IllegalArgumentException("malformed json, either null or no 'type' value");
@@ -40,13 +57,62 @@ public abstract class Condition {
             case "not":
                 cond = not(fromJSON(json.getJSONObject("val")));
                 break;
+            case "true":
+                cond = trueCondition();
+            case "false":
+                cond = falseCondition();
             default:
                 throw new IllegalArgumentException("Unexpected Item type (" + type + ")");
         }
         return cond;
     }
 
+    /**
+     * Create an always true condition
+     * @return true condition
+     */
+    public static Condition trueCondition()
+    {
+        return new Condition() {
 
+            @Override
+            public boolean matches() {
+                return true;
+            }
+
+            @Override
+            protected void compose(JSONObject json) throws JSONException {
+                json.accumulate("type", "true");
+            }
+        };
+    }
+
+    /**
+     * Create an always false condition
+     * @return false condition
+     */
+    public static Condition falseCondition()
+    {
+        return new Condition() {
+
+            @Override
+            public boolean matches() {
+                return false;
+            }
+
+            @Override
+            protected void compose(JSONObject json) throws JSONException {
+                json.accumulate("type", "false");
+            }
+        };
+    }
+
+    /**
+     * create a condition that represent the intersection of two conditions
+     * @param c1 first condition
+     * @param c2 second condition
+     * @return a new condition that is the intersection of c1 and c2
+     */
     public static Condition and(final Condition c1, final Condition c2)
     {
         return new Condition() {
@@ -64,6 +130,12 @@ public abstract class Condition {
         };
     }
 
+    /**
+     * create a condition that represent the union of two conditions
+     * @param c1 first condition
+     * @param c2 second condition
+     * @return a new condition that is the union of c1 and c2
+     */
     public static Condition or(final Condition c1, final Condition c2)
     {
         return new Condition() {
@@ -81,6 +153,11 @@ public abstract class Condition {
         };
     }
 
+    /**
+     * negats a condition
+     * @param c condition to negate
+     * @return a condition that is true when c is false and false when c is true
+     */
     public static Condition not(final Condition c)
     {
         return new Condition() {
@@ -97,6 +174,11 @@ public abstract class Condition {
         };
     }
 
+    /**
+     * A Builder for {@link Condition}, has no build() method since Item isn't instantiable,
+     * is used by the child builders (in {@link PositionCondition} or...) to build the "Condition
+     * part of the object". currently only used to parse JSON
+     */
     protected static class Builder {
 
         public Builder parse(JSONObject o) throws JSONException {
