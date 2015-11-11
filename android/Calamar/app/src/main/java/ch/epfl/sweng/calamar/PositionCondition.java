59dca669da6d1ae862a75e17c43b13db15c52c95
Pierre Quinton <pierre.quinton@epfl.ch>
2015-11-11 06:26:17 +0100

    corrected usage of GPSProvider.getInstance()

--- android/Calamar/app/src/main/java/ch/epfl/sweng/calamar/PositionCondition.java
+++ android/Calamar/app/src/main/java/ch/epfl/sweng/calamar/PositionCondition.java
@@ -40,7 +40,7 @@ public class PositionCondition extends Condition {
     {
         this.location = location;
         this.radius = radius;
-        GPSProvider.getInstance(null).addObserver(new GPSProvider.Observer() {
+        GPSProvider.getInstance().addObserver(new GPSProvider.Observer() {
 
             @Override
             public void update(Location newLocation) {
