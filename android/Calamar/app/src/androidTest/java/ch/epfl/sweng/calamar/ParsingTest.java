44d18d45588b585fb61c63efeb2f868e88b77204
Pierre Quinton <pierre.quinton@epfl.ch>
2015-11-18 17:10:59 +0100

    putted back test as it should be, should now fail one test. To fix this might try with raw ressources for test with a sample picture

--- android/Calamar/app/src/androidTest/java/ch/epfl/sweng/calamar/ParsingTest.java
+++ android/Calamar/app/src/androidTest/java/ch/epfl/sweng/calamar/ParsingTest.java
@@ -45,19 +45,7 @@ public class ParsingTest {
     public void testImageItemParsing() throws JSONException {
         // just a random 5x5 pgn pictures to test
         byte[] bytes = {(byte)0x89, (byte)0x50, (byte)0x4e, (byte)0x47, (byte)0x0d, (byte)0x0a, (byte)0x1a, (byte)0x0a, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x0d, (byte)0x49, (byte)0x48, (byte)0x44, (byte)0x52, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x05, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x05, (byte)0x08, (byte)0x02, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x02, (byte)0x0d, (byte)0xb1, (byte)0xb2, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x25, (byte)0x49, (byte)0x44, (byte)0x41, (byte)0x54, (byte)0x08, (byte)0x99, (byte)0x4d, (byte)0x8a, (byte)0xb1, (byte)0x0d, (byte)0x00, (byte)0x20, (byte)0x0c, (byte)0x80, (byte)0xc0, (byte)0xff, (byte)0x7f, (byte)0xc6, (byte)0xc1, (byte)0xc4, (byte)0x96, (byte)0x81, (byte)0x05, (byte)0xa8, (byte)0x80, (byte)0x67, (byte)0xe0, (byte)0xb0, (byte)0xa8, (byte)0xfc, (byte)0x65, (byte)0xba, (byte)0xaa, (byte)0xce, (byte)0xb3, (byte)0x97, (byte)0x0b, (byte)0x2b, (byte)0xd9, (byte)0x11, (byte)0xfa, (byte)0xa5, (byte)0xad, (byte)0x00, (byte)0x06, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x49, (byte)0x45, (byte)0x4e, (byte)0x44, (byte)0xae, (byte)0x42, (byte)0x60, (byte)0x82};
-
-
-        Bitmap b1 = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-
-        JSONObject json = new JSONObject();
-
-        json.accumulate("image", ImageItem.bitmap2String(b1));
-
-        Bitmap b2 = ImageItem.string2Bitmap(json.getString("image"));
-
-        assert(b1.sameAs(b2));
-
-        //helperItemParsing(new ImageItem(42, new User(69, "Jean Luc"), new User(666, "Albert Danton"), new Date(22313), Condition.trueCondition(),  BitmapFactory.decodeByteArray(bytes, 0, bytes.length)));
+        helperItemParsing(new ImageItem(42, new User(69, "Jean Luc"), new User(666, "Albert Danton"), new Date(22313), Condition.trueCondition(),  BitmapFactory.decodeByteArray(bytes, 0, bytes.length)));
     }
 
     @Test
