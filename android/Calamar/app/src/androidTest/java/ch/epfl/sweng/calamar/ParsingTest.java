0274973cca98e97bb2f3d6ecdb782b97640f7e7c
Pierre Quinton <pierre.quinton@epfl.ch>
2015-11-20 08:57:49 +0100

    seems that Instrumentation is not setUped right because I get a null pointer for my bitmap

--- android/Calamar/app/src/androidTest/java/ch/epfl/sweng/calamar/ParsingTest.java
+++ android/Calamar/app/src/androidTest/java/ch/epfl/sweng/calamar/ParsingTest.java
@@ -2,10 +2,11 @@ package ch.epfl.sweng.calamar;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.support.test.InstrumentationRegistry;
 import android.test.InstrumentationTestCase;
+import android.util.Log;
 
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,7 +22,6 @@ import ch.epfl.sweng.calamar.item.SimpleTextItem;
 import ch.epfl.sweng.calamar.recipient.Recipient;
 import ch.epfl.sweng.calamar.recipient.User;
 
-import static org.junit.Assert.assertEquals;
 
 /**
  * Created by pierre on 10/20/15.
@@ -29,7 +29,23 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(JUnit4.class)
 public class ParsingTest extends InstrumentationTestCase {
+    private static final String TAG = "ParsingTest";
 
+    public ParsingTest() throws Exception {
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        Log.d(TAG, "setUp(): " + getName());
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        Log.d(TAG, "tearDown(): " + getName());
+        super.tearDown();
+    }
 
     @Test
     public void testUserParsing() throws JSONException {
@@ -45,6 +61,7 @@ public class ParsingTest extends InstrumentationTestCase {
     @Test
     public void testImageItemParsing() throws JSONException {
         Bitmap bitmap = BitmapFactory.decodeResource(this.getInstrumentation().getContext().getResources(), R.drawable.in_message_bg);
+        assertNotNull(bitmap);
         helperItemParsing(new ImageItem(42, new User(69, "Jean Luc"), new User(666, "Albert Danton"), new Date(22313), Condition.trueCondition(), bitmap));
     }
 
