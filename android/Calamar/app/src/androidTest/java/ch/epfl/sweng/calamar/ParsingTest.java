f777ac00c98177f80c91da38a548293fcbd4fdb2
Pierre Quinton <pierre.quinton@epfl.ch>
2015-11-20 08:37:14 +0100

    trying by making test a subclass of InstrumentationTestCase, can getContext and R.drawable

--- android/Calamar/app/src/androidTest/java/ch/epfl/sweng/calamar/ParsingTest.java
+++ android/Calamar/app/src/androidTest/java/ch/epfl/sweng/calamar/ParsingTest.java
@@ -2,6 +2,7 @@ package ch.epfl.sweng.calamar;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.test.InstrumentationTestCase;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -27,7 +28,7 @@ import static org.junit.Assert.assertEquals;
  */
 
 @RunWith(JUnit4.class)
-public class ParsingTest {
+public class ParsingTest extends InstrumentationTestCase {
 
 
     @Test
@@ -43,9 +44,8 @@ public class ParsingTest {
 
     @Test
     public void testImageItemParsing() throws JSONException {
-        // just a random 5x5 pgn pictures to test
-        byte[] bytes = {(byte)0x89, (byte)0x50, (byte)0x4e, (byte)0x47, (byte)0x0d, (byte)0x0a, (byte)0x1a, (byte)0x0a, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x0d, (byte)0x49, (byte)0x48, (byte)0x44, (byte)0x52, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x05, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x05, (byte)0x08, (byte)0x02, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x02, (byte)0x0d, (byte)0xb1, (byte)0xb2, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x25, (byte)0x49, (byte)0x44, (byte)0x41, (byte)0x54, (byte)0x08, (byte)0x99, (byte)0x4d, (byte)0x8a, (byte)0xb1, (byte)0x0d, (byte)0x00, (byte)0x20, (byte)0x0c, (byte)0x80, (byte)0xc0, (byte)0xff, (byte)0x7f, (byte)0xc6, (byte)0xc1, (byte)0xc4, (byte)0x96, (byte)0x81, (byte)0x05, (byte)0xa8, (byte)0x80, (byte)0x67, (byte)0xe0, (byte)0xb0, (byte)0xa8, (byte)0xfc, (byte)0x65, (byte)0xba, (byte)0xaa, (byte)0xce, (byte)0xb3, (byte)0x97, (byte)0x0b, (byte)0x2b, (byte)0xd9, (byte)0x11, (byte)0xfa, (byte)0xa5, (byte)0xad, (byte)0x00, (byte)0x06, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x49, (byte)0x45, (byte)0x4e, (byte)0x44, (byte)0xae, (byte)0x42, (byte)0x60, (byte)0x82};
-        helperItemParsing(new ImageItem(42, new User(69, "Jean Luc"), new User(666, "Albert Danton"), new Date(22313), Condition.trueCondition(),  BitmapFactory.decodeByteArray(bytes, 0, bytes.length)));
+        Bitmap bitmap = BitmapFactory.decodeResource(this.getInstrumentation().getContext().getResources(), R.drawable.in_message_bg);
+        helperItemParsing(new ImageItem(42, new User(69, "Jean Luc"), new User(666, "Albert Danton"), new Date(22313), Condition.trueCondition(), bitmap));
     }
 
     @Test
