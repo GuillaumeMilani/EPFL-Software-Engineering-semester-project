cce9f4bcde58eedd16c075626049f8c56364ab34
Guillaume Milani <guillaume.milani@epfl.ch>
2015-11-20 16:34:16 +0100

    Forgotten this file in precedent commit

--- server/controllers/items/send_item.php
+++ server/controllers/items/send_item.php
@@ -1,5 +1,6 @@
 <?php
 include_once('models/items/add_items.php');
+include_once('models/conditions/add_conditions.php');
 
 // Retrieve post data
 $content = urldecode(file_get_contents('php://input'));
@@ -14,19 +15,27 @@ if($data == null)
 else
 {
 	// extract decoded data
-	if (isset($data['message']) && isset($data['type']) && isset($data['from']) && isset($data['to'])) {
+	if (isset($data['message']) && isset($data['type']) && isset($data['from']) && isset($data['to']) && isset($data['condition'])) {
 			$item = $data['message'];
 			$type = $data['type'];
 			$from = $data['from'];
 			$to = $data['to'];
 			$date = $data['date'];
+			$condition = $data['condition'];
 	} else {
 		http_response_code(400);
 		die("Error : json malformed");
 	}
-
+	
+	if ($condition['type'] == "true") {
+		$condition_id = -1;
+	} else {
+		$condition_id = add_conditions($condition);
+	}
 	// add the data into the db
-	if(add_items($from['ID'],$to['ID'],$date,$type,$item))
+	$result = add_items($from['ID'],$to['ID'],$date,$type,$item,$condition_id);
+	
+	if($result)
 	{
 		http_response_code(201);
 	}
