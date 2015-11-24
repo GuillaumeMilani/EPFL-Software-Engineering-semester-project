4652160ca33cee5b8b0187cefe9910e6153032a7
Guillaume Milani <guillaume.milani@epfl.ch>
2015-11-24 10:40:32 +0100

    Get and set items with condition

--- server/controllers/items/send_item.php
+++ server/controllers/items/send_item.php
@@ -1,47 +1,59 @@
 <?php
 include_once('models/items/add_items.php');
 include_once('models/conditions/add_conditions.php');
+global $pdo;
 
-// Retrieve post data
-$content = urldecode(file_get_contents('php://input'));
-// decode the json
-$data = json_decode($content, true);
-//check if there is an error
-if($data == null)
-{
+$data = get_post_JSON();
+
+// extract decoded data
+if (isset($data['message']) && isset($data['type']) && isset($data['from']) && isset($data['to']) && isset($data['condition'])) {
+		$item = $data['message'];
+		$type = $data['type'];
+		$from = $data['from'];
+		$to = $data['to'];
+		$date = time();
+		$condition = $data['condition'];
+} else {
 	http_response_code(400);
-	die("Error : json data not found");
+	die("Error : json malformed");
+}
+
+$condition_id = -1;
+if ($condition['type'] == "true") {
+	$condition_id = null;
+} else {
+	if (isset($condition['metadata'])) {
+			$metadata = $condition['metadata'];
+			unset($condition['metadata']);
+			$condition = json_encode($condition);
+	}
+	
+	$condition_id = add_condition($condition);
+	
+	foreach ($metadata as $data)
+	{
+		if ($data['type'] == "position" && isset($data['latitude']) && isset($data['longitude']) && isset($data['radius'])) {
+			if (!add_metadata_position($condition_id, $data['latitude'], $data['longitude'], $data['radius'])) {
+				http_response_code(500);
+				die("Error : database");
+			}
+		} else {
+			http_response_code(400);
+			die("Error : json malformed");
+		}
+	}
+}
+
+// add the data into the db
+$result = add_items($from['ID'],$to['ID'],$date,$type,$item,$condition_id);
+
+if($result)
+{
+	http_response_code(201);
+	echo json_encode(array("type" => $type, "from" => $from, "to" => $to, "date" => $date, "condition" => $condition));
 }
 else
 {
-	// extract decoded data
-	if (isset($data['message']) && isset($data['type']) && isset($data['from']) && isset($data['to']) && isset($data['condition'])) {
-			$item = $data['message'];
-			$type = $data['type'];
-			$from = $data['from'];
-			$to = $data['to'];
-			$date = $data['date'];
-			$condition = $data['condition'];
-	} else {
-		http_response_code(400);
-		die("Error : json malformed");
-	}
-	
-	if ($condition['type'] == "true") {
-		$condition_id = -1;
-	} else {
-		$condition_id = add_conditions($condition);
-	}
-	// add the data into the db
-	$result = add_items($from['ID'],$to['ID'],$date,$type,$item,$condition_id);
-	
-	if($result)
-	{
-		http_response_code(201);
-	}
-	else
-	{
-		http_response_code(500);
-		die("Error : database");
-	}
+	http_response_code(500);
+	die("Error : database");
 }
--- server/models/items/get_items.php
+++ server/models/items/get_items.php
@@ -3,13 +3,16 @@
 function get_item_with_location($recipient, $last_refresh, $latitude, $longitude, $radius) {
 	global $pdo;
 	
-	$res = $pdo->prepare('SELECT 
+	$res = $pdo->prepare('
+			SELECT DISTINCT
 				itm.`ID`, itm.`from`, itm.`to`, itm.`date`, cnd.`condition`, txt.`text`, "simpleText" as "type"
     		FROM 
 				tb_item as itm, tb_item_text as txt, tb_condition as cnd, tb_metadata as mtd, tb_metadata_position as pos
-    		WHERE
-				pos.latitude <= :latitude
-			AND pos.longitude <= :longitude
+			WHERE
+				pos.latitude <= :latitude_max
+			AND pos.latitude >= :latitude_min
+			AND pos.longitude <= :longitude_max
+			AND pos.longitude >= :longitude_min
 			AND pos.ID = mtd.ID
 			AND mtd.ID = cnd.ID
 			AND itm.condition = cnd.ID
@@ -17,8 +20,10 @@ function get_item_with_location($recipient, $last_refresh, $latitude, $longitude
     		AND itm.date > :last_refresh
 			AND itm.ID = txt.ID');
 	
-	$res->bindParam(':latitude', $latitude, PDO::PARAM_STR);
-	$res->bindParam(':longitude', $latitude, PDO::PARAM_STR);
+	$res->bindParam(':latitude_min', $latitude - $radius, PDO::PARAM_STR);
+	$res->bindParam(':latitude_max', $latitude + $radius, PDO::PARAM_STR);
+	$res->bindParam(':longitude_min', $latitude - $radius, PDO::PARAM_STR);
+	$res->bindParam(':longitude_max', $latitude + $radius, PDO::PARAM_STR);
 	$res->bindParam(':to', $recipient['ID'], PDO::PARAM_INT);
 	$res->bindParam(':last_refresh', $last_refresh, PDO::PARAM_STR);
 	$res->execute();
@@ -61,13 +66,18 @@ function get_user($user_id) {
  * @param posix_time $last_refresh
  * @return array of items (indexed by column name)
  */
-function get_items($recipient, $last_refresh) {
+function get_items($recipient, $last_refresh, $get_content = false) {
 	global $pdo;
 	
+	if ($get_content) {
+		$table = "view_text_message";
+	} else {
+		$table = "tb_item";
+	}
 	$res = $pdo->prepare('SELECT *, "simpleText" as "type"
-    		FROM view_text_message as msg
-    		WHERE msg.to = :to
-    		AND msg.date > :last_refresh');
+    		FROM '.$table.' as itm
+    		WHERE itm.to = :to
+    		AND itm.date > :last_refresh');
 	
 	$res->bindParam(':to', $recipient['ID'], PDO::PARAM_INT);
 	$res->bindParam(':last_refresh', $last_refresh, PDO::PARAM_STR);
@@ -83,4 +93,8 @@ function get_items($recipient, $last_refresh) {
 	}
 	
 	return $ret;
+}
+
+function get_items_with_content($recipient, $last_refresh) {
+	get_items($recipient, $last_refresh, true);
 }
