762162b8bbbe249a40c927c9e0a73773739ba1c5
Lucas Pires <lucas.pires@epfl.ch>
2015-12-02 19:47:07 +0100

    fix comment

--- android/Calamar/app/src/main/java/ch/epfl/sweng/calamar/client/NetworkDatabaseClient.java
+++ android/Calamar/app/src/main/java/ch/epfl/sweng/calamar/client/NetworkDatabaseClient.java
@@ -89,7 +89,7 @@ public class NetworkDatabaseClient implements DatabaseClient {
 
             connection = NetworkDatabaseClient.createConnection(networkProvider, url);
             String response = NetworkDatabaseClient.post(connection, jsonParameter.toString());
-            Log.v(NetworkDatabaseClient.TAG, response);
+            // Log.v(NetworkDatabaseClient.TAG, response);
             JSONObject object = new JSONObject(response);
             return object.getInt("ID");
         } catch (IOException | JSONException e) {
@@ -144,7 +144,7 @@ public class NetworkDatabaseClient implements DatabaseClient {
             connection = NetworkDatabaseClient.createConnection(networkProvider, url);
             //Log.v(TAG, jsonParameter.toString());
             String response = NetworkDatabaseClient.post(connection, jsonParameter.toString());
-            Log.v(TAG, response);
+            //Log.v(TAG, response);
             return NetworkDatabaseClient.itemsFromJSON(response);
         } catch (IOException | JSONException e) {
             throw new DatabaseClientException(e);
