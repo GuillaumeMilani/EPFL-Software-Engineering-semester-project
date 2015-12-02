f5f1892d5cead03f2af9e34f6c6a5babdf5b0ca4
Guillaume Milani <guillaume.milani@epfl.ch>
2015-12-02 17:43:17 +0100

    Multiple item type retrieve

--- server/models/items/get_items.php
+++ server/models/items/get_items.php
@@ -1,15 +1,62 @@
 <?php
 include('models/users/retrieve_users.php');
 
+define('TYPES', array(
+		array('name' => 'SIMPLETEXTITEM', 'table' => 'tb_item_text', 'fields' => ''),
+		array('name' => 'IMAGEITEM', 'table' => 'tb_item_image', 'fields' => ', type.`data`'),
+		array('name' => 'FILEITEM', 'table' => 'tb_item_file', 'fields' => ', type.`data`'),
+));
+
 // List of the fields to select in the DB for an item
-const TO_BE_SELECTED = 'itm.`ID`, itm.`from`, itm.`to`, itm.`date`, cnd.`condition`, itm.`message`, "SIMPLETEXTITEM" as "type"';
+define('LOCATION_WHERE','
+		    AND pos.latitude <= :latitude_max
+			AND pos.latitude >= :latitude_min
+			AND pos.longitude <= :longitude_max
+			AND pos.longitude >= :longitude_min
+			AND pos.ID = mtd.ID
+			AND mtd.condition = cnd.ID
+			AND	(itm.to = :to OR itm.to IS NULL)');
+    		
+define('LOCATION_FROM',', tb_metadata as mtd, tb_metadata_position as pos');
 
 /**
  * Execute a query to get items and replace the "from" and "to" by a JSON Object corresponding to users
  * @param $query a prepared query that contains at least SELECT `from`
  * @return array of items (indexed by column name)
  */
-function get_items($query) {
+function get_items($recipient, $last_refresh, $type, $location_params = '') {
+	$location_where = '';
+	$location_from = '';
+	
+	if (isset($location_params)) {
+		$location_where = LOCATION_WHERE;
+		$location_from = LOCATION_FROM;
+	}
+	
+	$query = $pdo->prepare('
+			SELECT
+				itm.`ID`, itm.`from`, itm.`to`, itm.`date`, cnd.`condition`, itm.`message` '.$type['fields'].','.$type['name'].' as "type"
+    		FROM
+				tb_item as itm, '.$type['table'].' as type, tb_condition as cnd '.$location_from.'
+			WHERE
+				itm.ID = type.ID
+			AND	itm.condition = cnd.ID
+			AND	itm.to = :to
+    		AND itm.date > :last_refresh
+			'.$location_where);
+	
+	$id = $recipient['ID'];
+	
+	$query->bindParam(':to', $id, PDO::PARAM_INT);
+	$query->bindParam(':last_refresh', $last_refresh, PDO::PARAM_STR);
+	
+	if (isset($location_params)) {
+		$query->bindParam(':latitude_min', $location_params['latitude_min'], PDO::PARAM_STR);
+		$query->bindParam(':latitude_max', $location_params['latitude_max'], PDO::PARAM_STR);
+		$query->bindParam(':longitude_min', $location_params['longitude_min'], PDO::PARAM_STR);
+		$query->bindParam(':longitude_max', $location_params['longitude_max'], PDO::PARAM_STR);
+	}
+	
 	$query->execute();
 
 	$ret = [];
@@ -39,32 +86,20 @@ function get_item_with_location($recipient, $last_refresh, $latitude_min, $latit
 		$longitude_min, $longitude_max) {
 	global $pdo;
 	
-	$res = $pdo->prepare('
-			SELECT DISTINCT
-				'.TO_BE_SELECTED.'
-    		FROM 
-				tb_item as itm, tb_item_text as txt, tb_condition as cnd, tb_metadata as mtd, tb_metadata_position as pos
-			WHERE
-				pos.latitude <= :latitude_max
-			AND pos.latitude >= :latitude_min
-			AND pos.longitude <= :longitude_max
-			AND pos.longitude >= :longitude_min
-			AND pos.ID = mtd.ID
-			AND mtd.condition = cnd.ID
-			AND itm.condition = cnd.ID
-			AND	(itm.to = :to OR itm.to IS NULL)
-    		AND itm.date > :last_refresh
-			AND itm.ID = txt.ID');
+	$location_params = array(
+			'latitude_min' => $latitude_min,
+			'latitude_max' => $latitude_max,
+			'longitude_min' => $longitude_min,
+			'longitude_max' => $longitude_max
+	);
 	
-	$recipient_id = $recipient['ID'];
-	$res->bindParam(':latitude_min', $latitude_min, PDO::PARAM_STR);
-	$res->bindParam(':latitude_max', $latitude_max, PDO::PARAM_STR);
-	$res->bindParam(':longitude_min', $longitude_min, PDO::PARAM_STR);
-	$res->bindParam(':longitude_max', $longitude_max, PDO::PARAM_STR);
-	$res->bindParam(':to', $recipient_id, PDO::PARAM_INT);
-	$res->bindParam(':last_refresh', $last_refresh, PDO::PARAM_STR);
+	$ret = array();
 	
-	return get_items($res);
+	foreach (TYPES as $type) {
+		$ret = array_merge($ret,get_items($recipient, $last_refresh, $type, $location_params));
+	}
+	
+	return $ret;
 }
 
 /*
@@ -73,21 +108,11 @@ function get_item_with_location($recipient, $last_refresh, $latitude_min, $latit
 function get_all_private_items($recipient, $last_refresh) {
 	global $pdo;
 	
-	$query = $pdo->prepare('
-			SELECT
-				'.TO_BE_SELECTED.'
-    		FROM
-				tb_item as itm, tb_item_text as txt, tb_condition as cnd
-			WHERE
-				itm.condition = cnd.ID
-			AND	itm.to = :to
-    		AND itm.date > :last_refresh
-			AND itm.ID = txt.ID');
-			
-	$id = $recipient['ID'];
+	$ret = array();
 	
-	$query->bindParam(':to', $id, PDO::PARAM_INT);
-	$query->bindParam(':last_refresh', $last_refresh, PDO::PARAM_STR);
+	foreach (TYPES as $type) {
+		$ret = array_merge($ret,get_items($recipient, $last_refresh, $type));
+	}
 	
-	return get_items($query);
+	return $ret;
 }
